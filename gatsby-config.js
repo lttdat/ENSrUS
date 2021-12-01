21942143714ac4f8bce8b741be6f57a6d51e0294
lttdat <84722589+lttdat@users.noreply.github.com>
2021-12-02 02:55:44 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `dat.ETH`,
+    title: `dat.eth`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@dat.eth`,
     siteUrl: `https://dat.eth`,
