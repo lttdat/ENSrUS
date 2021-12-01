aa8791aa5166c6f2e651816ee5440c7e61d8d97d
lttdat <84722589+lttdat@users.noreply.github.com>
2021-12-01 23:57:07 +0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main><iframe src='https://opensea.io/wanderer89?embed=true'
+        <main><iframe src='https://opensea.io/dat_eth?embed=true'
         width='100%'
         height='1400px'
         frameborder='0'
