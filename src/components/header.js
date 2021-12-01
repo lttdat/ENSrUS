7c252ac48636a33d661a70c0c807cdac2441da47
lttdat <84722589+lttdat@users.noreply.github.com>
2021-12-02 03:46:26 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin-left: `auto` }}>
+      <h1 style={{ margin-left: `auto`, }}>
         <Link
           to="/"
           style={{
