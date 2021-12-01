e4eb940096c37689a0b868645aff1fa24aa4a115
lttdat <84722589+lttdat@users.noreply.github.com>
2021-12-02 02:38:28 +0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,7 +45,7 @@ const Layout = ({ children }) => {
         >
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://opensea.io/dat_eth">love</a>
         </footer>
       </div>
     </>
