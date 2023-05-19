8411987a687d4eb8fae635f83c7aec8b2318dad2
moonmaker-xyz <84722589+moonmaker-xyz@users.noreply.github.com>
2023-05-19 21:39:27 +0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main><iframe src='src/images/gatsby-icon.png'
+        <main><iframe src='https://moonmaker.xyz/banner.jpg'
         width='100%'
         height='1400px'
         frameborder='0'
